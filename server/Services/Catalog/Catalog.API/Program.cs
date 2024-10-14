7b5ce452a6511ffd49fbf026e9ab548698a69f6d
anshulghogre4 <anshulghogre4@gmail.com>
2024-10-14 21:09:30 +0530

    Catalog API started

--- server/Services/Catalog/Catalog.API/Program.cs
+++ server/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //ADD Services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof (Program).Assembly);
+});
 
 var app = builder.Build();
 
 // config the http request pipeline
-
+app.MapCarter();
 app.Run();
