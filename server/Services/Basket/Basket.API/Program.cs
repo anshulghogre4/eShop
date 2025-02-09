0bb4a86af3c23c535860080998900b7b76caf0d2
“anshulghogre4” <anshulghogre4@gmail.com>
2025-02-10 01:56:43 +0530

    Register basket APIs

--- server/Services/Basket/Basket.API/Program.cs
+++ server/Services/Basket/Basket.API/Program.cs
@@ -1,10 +1,21 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
+
+builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+});
 
 var app = builder.Build();
 
 //configure the http request pipeline
 
-
+app.MapCarter();
 app.Run();
