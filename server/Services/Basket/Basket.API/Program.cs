7186ce5bbb0df041eebdaf696f53549d5e865810
“anshulghogre4” <anshulghogre4@gmail.com>
2025-02-13 01:53:21 +0530

    added repository pattern approach with marten  and dockerised the database

--- server/Services/Basket/Basket.API/Program.cs
+++ server/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,7 @@
 
 
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
@@ -12,10 +14,17 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
     config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
 });
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
 //configure the http request pipeline
-
 app.MapCarter();
+app.UseExceptionHandler(options => { });
+//app.UseHealthChecks("/health", new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse });
+
 app.Run();
