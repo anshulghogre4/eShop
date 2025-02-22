4c9396391c45f8ad5e0c7cd14516f7a65cda972f
“anshulghogre4” <anshulghogre4@gmail.com>
2025-02-23 00:54:29 +0530

    grpc cruds

--- server/Services/Discount/Discount.Grpc/Program.cs
+++ server/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,3 @@
-using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +8,10 @@ builder.Services.AddGrpcReflection();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-if (app.Environment.IsDevelopment()) app.MapGrpcReflectionService();
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 app.Run();
