089b12ad58b62da31364b0902f52c771ba8361fa
“anshulghogre4” <anshulghogre4@gmail.com>
2025-03-06 22:22:15 +0530

    Started Ordering setup and understanding of clean architecture

---
+++ server/Services/Ordering/Ordering.API/DependecyInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.API
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddAPIServices(this IServiceCollection services)
+        {
+            // register services realted to API layer
+            return services;
+        }
+
+        public static WebApplication UseAPIServices(this WebApplication app)
+        {
+            // use services realted to API layer
+            return app;
+        }
+    }
+}
--- server/Services/Ordering/Ordering.API/Program.cs
+++ server/Services/Ordering/Ordering.API/Program.cs
@@ -1,9 +1,16 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services
+    .AddApplicaionServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddAPIServices();
 
 var app = builder.Build();
 
-
+app.UseAPIServices();
 
 app.Run();
