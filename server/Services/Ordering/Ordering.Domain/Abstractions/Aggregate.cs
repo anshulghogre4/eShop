023f23b56dc426d97e76fd81b4dd3bff887b70a2
“anshulghogre4” <anshulghogre4@gmail.com>
2025-03-07 01:19:19 +0530

    laid foundation for DDD

---
+++ server/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new(); //= new(); – This is a shorthand way of writing new List<IDomainEvent>(); introduced in C# 9.0.
+        IReadOnlyList<IDomainEvent> IAggregate.DomainEvents => throw new NotImplementedException();
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        IDomainEvent[] IAggregate.ClearDomainEvents()
+        {
+            IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+            _domainEvents.Clear();
+            return dequeuedEvents;
+        }
+    }
+}
---
+++ server/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿
+using MediatR;
+
+namespace Ordering.Domain.Abstractions
+{
+    public interface IDomainEvent : INotification
+    {
+        //The => (expression-bodied member) syntax is used to define a default interface member implementation in C# 8.0 and later.
+        Guid EventId => Guid.NewGuid(); //expression-bodied member. means it's aread only  or get only properties 
+        public DateTime OccurredOn  => DateTime.UtcNow;
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
